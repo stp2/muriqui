eca3aa283c0bf3413af1cc828dfc5ad49124736a
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2024-10-07 19:38:35 +0200

    add table users

--- create.sql
+++ create.sql
@@ -1,15 +1,22 @@
 -- sqlite thing for foreign keys support
 PRAGMA foreign_keys = ON;
 -- Remove conflicting tables
-DROP TABLE IF EXISTS cleni CASCADE;
-DROP TABLE IF EXISTS porada CASCADE;
-DROP TABLE IF EXISTS schuzky CASCADE;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS cleni;
+DROP TABLE IF EXISTS porada;
+DROP TABLE IF EXISTS schuzky;
 -- End of removing
 
 CREATE TABLE cleni (
     id INTEGER PRIMARY KEY,
     discord_id TEXT NOT NULL UNIQUE,
     jmeno TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    role INTEGER NOT NULL DEFAULT 1,
+    FOREIGN KEY(id) REFERENCES cleni(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE schuzky (
@@ -20,8 +27,10 @@ CREATE TABLE schuzky (
 );
 
 CREATE TABLE porada (
-    cleni_id INTEGER PRIMARY KEY NOT NULL,
-    schuzky_id INTEGER PRIMARY KEY NOT NULL,
+    cleni_id INTEGER NOT NULL,
+    schuzky_id INTEGER NOT NULL,
     zprava_id TEXT NOT NULL UNIQUE DEFAULT '',
-    FOREIGN KEY(cleni_id, schuzky_id) REFERENCES cleni(id), schuzky(id) ON DELETE CASCADE ON UPDATE CASCADE
+    PRIMARY KEY(cleni_id, schuzky_id),
+    FOREIGN KEY(cleni_id) REFERENCES cleni(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(schuzky_id) REFERENCES schuzky(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
